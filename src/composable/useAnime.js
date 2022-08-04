ffd86589bbd0d99cbf333b6cd6c22d727e8aa656
Jesus Morales <jesusalbertomoralesrico@gmail.com>
2022-08-04 18:40:14 -0500

    Feature: Created useAnime hook

---
+++ src/composable/useAnime.js
@@ -0,0 +1,31 @@
+import axios from "axios";
+import { storeToRefs } from "pinia";
+import { useStore } from "@/store";
+
+export const useAnime = () => {
+  const animeStore = useStore();
+
+  const { animes } = storeToRefs(animeStore);
+
+  const ANIME_API_URL = "https://api.jikan.moe/v4/anime";
+
+  /* Fetch anime data and setup the store */
+
+  const fetchAnimeList = () => {
+    axios
+      .get("https://api.jikan.moe/v4/anime")
+      .then((response) => onAnimeListResponse(response));
+  };
+
+  const onAnimeListResponse = (response) => {
+    animeStore.setAnimes(response.data.data);
+  };
+
+  return {
+    // Propierties
+    animes,
+
+    // Methods
+    fetchAnimeList,
+  };
+};
