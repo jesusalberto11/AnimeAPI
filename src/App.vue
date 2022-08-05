b3a469715e102440628264d301f57167a3a0a0ae
Jesus Morales <jesusalbertomoralesrico@gmail.com>
2022-08-04 21:04:02 -0500

    Update: Created Basic layout

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,65 @@
 <template>
-  <h1>Hello world!</h1>
+  <div class="app-container">
+    <AppHeader />
+    <nav class="sidebar">Sidebar</nav>
+    <main class="main-content">
+      Animes:
+      <div v-if="animes">
+        <div v-for="anime in animes" v-bind:key="anime.malId">
+          <li>
+            {{ anime.title }}
+          </li>
+        </div>
+      </div>
+    </main>
+  </div>
 </template>
 
-<script setup></script>
+<script setup>
+import { onMounted } from "vue";
+import { useAnime } from "./composable/useAnime";
 
-<style></style>
+import AppHeader from "./components/layout/AppHeader.vue";
+
+const { animes, fetchAnimeList } = useAnime();
+
+onMounted(() => {
+  fetchAnimeList();
+});
+</script>
+
+<style>
+* {
+  padding: 0;
+  margin: 0;
+  box-sizing: border-box;
+}
+
+body {
+  color: #fff;
+  font-family: "Roboto Mono";
+}
+
+.app-container {
+  width: 100%;
+  min-height: 100vh;
+  margin: 0 auto;
+  display: grid;
+  grid-template-columns: 90px auto;
+  grid-template-rows: 60px auto;
+  grid-template-areas:
+    "header header"
+    "sidebar content";
+  transition: all 0.3s ease;
+}
+
+.sidebar {
+  grid-area: sidebar;
+  background-color: antiquewhite;
+}
+
+.main-content {
+  grid-area: content;
+  background-color: brown;
+}
+</style>
