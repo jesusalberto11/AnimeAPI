c0c4bf82214138c7d607f08feb6e869c25cb0099
Jesus Morales <jesusalbertomoralesrico@gmail.com>
2022-08-05 18:41:30 -0500

    Feature: Created initial pages

---
+++ src/pages/AnimeListPage.vue
@@ -0,0 +1,7 @@
+<template>
+  <h1>Hello world from anime list page!</h1>
+</template>
+
+<script setup></script>
+
+<style scoped></style>
---
+++ src/pages/PathNotFound.vue
@@ -0,0 +1,7 @@
+<template>
+  <h1>Hello world from path not found</h1>
+</template>
+
+<script setup></script>
+
+<style scoped></style>
