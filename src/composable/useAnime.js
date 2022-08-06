505afacb899ce057f76fc005a3b903402575cd4b
Jesus Morales <jesusalbertomoralesrico@gmail.com>
2022-08-05 20:48:04 -0500

    Refractor: Updated get animes url to match a especific page

--- src/composable/useAnime.js
+++ src/composable/useAnime.js
@@ -7,14 +7,14 @@ export const useAnime = () => {
 
   const { animes, anime, isLoading } = storeToRefs(animeStore);
 
-  const ANIME_API_URL = "https://api.jikan.moe/v4/anime?q=&page=1";
-
   /* Fetch anime data and setup in the store */
 
-  const fetchAnimeList = () => {
+  const fetchAnimeList = (page = 1) => {
     animeStore.setLoading(true);
+    let ANIMES_API_URL = `https://api.jikan.moe/v4/anime?q=&page=${page}`;
+
     axios
-      .get(ANIME_API_URL)
+      .get(ANIMES_API_URL)
       .then((response) => onAnimeListResponse(response))
       .catch((error) => console.log("Error in fetchAnimeList: ", error));
   };
@@ -29,6 +29,7 @@ export const useAnime = () => {
   const fetchAnime = (id) => {
     animeStore.setLoading(true);
     let animeURL = `https://api.jikan.moe/v4/anime/${id}/full`;
+
     axios
       .get(animeURL)
       .then((response) => onAnimeResponse(response))
