cd270cca4f3c674abe30c9dfbeeaf2e9acb05e55
Jesus Morales <jesusalbertomoralesrico@gmail.com>
2022-08-06 18:03:28 -0500

    Feature: Created search results page route

--- src/router/index.js
+++ src/router/index.js
@@ -3,6 +3,7 @@ import * as VueRouter from "vue-router";
 import HomePage from "../pages/HomePage.vue";
 import AnimeListPage from "../pages/AnimeListPage.vue";
 import AnimePage from "../pages/AnimePage";
+import SearchResultsPage from "../pages/SearchResultsPage.vue";
 import PathNotFound from "../pages/PathNotFound.vue";
 
 const routes = [
@@ -10,6 +11,7 @@ const routes = [
   { path: "/home", name: "home", component: HomePage },
   { path: "/animes", name: "animes", component: AnimeListPage },
   { path: "/animes/anime/:id", name: "anime", component: AnimePage },
+  { path: "/results", name: "results", component: SearchResultsPage },
   { path: "/:pathMatch(.*)*", name: "not-found", component: PathNotFound },
 ];
 
