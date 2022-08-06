d2b311d5bc76ba9647973c445b1dbf9accb90855
Jesus Morales <jesusalbertomoralesrico@gmail.com>
2022-08-05 19:55:27 -0500

    Refractor: Added loading behavior to fetch functions

--- src/composable/useAnime.js
+++ src/composable/useAnime.js
@@ -5,36 +5,46 @@ import { useStore } from "@/store";
 export const useAnime = () => {
   const animeStore = useStore();
 
-  const { animes, anime } = storeToRefs(animeStore);
+  const { animes, anime, isLoading } = storeToRefs(animeStore);
 
   const ANIME_API_URL = "https://api.jikan.moe/v4/anime?q=&page=1";
 
   /* Fetch anime data and setup in the store */
 
-  const fetchAnimeList = async () => {
-    axios.get(ANIME_API_URL).then((response) => onAnimeListResponse(response));
+  const fetchAnimeList = () => {
+    animeStore.setLoading(true);
+    axios
+      .get(ANIME_API_URL)
+      .then((response) => onAnimeListResponse(response))
+      .catch((error) => console.log("Error in fetchAnimeList: ", error));
   };
 
   const onAnimeListResponse = (response) => {
-    console.log(response.data);
     animeStore.setAnimes(response.data.data);
+    animeStore.setLoading(false);
   };
 
   /* Fetch single anime data and setup in the store */
 
-  const fetchAnime = async (id) => {
+  const fetchAnime = (id) => {
+    animeStore.setLoading(true);
     let animeURL = `https://api.jikan.moe/v4/anime/${id}/full`;
-    axios.get(animeURL).then((response) => onAnimeResponse(response));
+    axios
+      .get(animeURL)
+      .then((response) => onAnimeResponse(response))
+      .catch((error) => console.log("Error in fetchAnime: ", error));
   };
 
   const onAnimeResponse = (response) => {
-    console.log(response);
+    animeStore.setAnime(response.data.data);
+    animeStore.setLoading(false);
   };
 
   return {
     // Propierties
     animes,
     anime,
+    isLoading,
 
     // Methods
     fetchAnimeList,
