492fe56a1412044bded23d30e671b0a2c1b26895
Jesus Morales <jesusalbertomoralesrico@gmail.com>
2022-08-06 18:03:17 -0500

    Feature: Created search results page

---
+++ src/pages/SearchResultsPage.vue
@@ -0,0 +1,11 @@
+<template>
+  <h1>Hello world serchresults page! {{ route.params.query }}</h1>
+</template>
+
+<script setup>
+import { useRoute } from "vue-router";
+
+const route = useRoute();
+</script>
+
+<style scoped></style>
