700b27aaed6fa8ee004d38d1648c3ccf9fa9b0d5
Jesus Morales <jesusalbertomoralesrico@gmail.com>
2022-08-05 19:26:37 -0500

    Feature: Created anime page

---
+++ src/pages/AnimePage.vue
@@ -0,0 +1,16 @@
+<template>
+  <h1>Hello world anime page!</h1>
+</template>
+
+<script setup>
+import { onMounted } from "vue";
+import { useRoute } from "vue-router";
+
+const route = useRoute();
+
+onMounted(() => {
+  console.log("id es: ", route.params?.id);
+});
+</script>
+
+<style scoped></style>
