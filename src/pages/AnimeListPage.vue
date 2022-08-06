604139473531f46d723eb7ccac8a5874f1adcbb0
Jesus Morales <jesusalbertomoralesrico@gmail.com>
2022-08-05 19:08:49 -0500

    Update: Created anime list page basic layout

--- src/pages/AnimeListPage.vue
+++ src/pages/AnimeListPage.vue
@@ -1,7 +1,33 @@
 <template>
-  <h1>Hello world from anime list page!</h1>
+  <div class="page-container">
+    <div v-if="animes.length === 0">
+      <h1>Loading...</h1>
+    </div>
+    <div v-else>
+      <AnimeList :animes="animes" />
+    </div>
+  </div>
 </template>
 
-<script setup></script>
+<script setup>
+import { onMounted } from "vue";
+import { useAnime } from "@/composable/useAnime.js";
+import AnimeList from "../components/AnimeList/AnimeList.vue";
 
-<style scoped></style>
+const { animes, fetchAnimeList } = useAnime();
+
+onMounted(() => {
+  fetchAnimeList();
+});
+</script>
+
+<style scoped>
+.page-container {
+  height: 100%;
+  width: 100%;
+
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+</style>
