a62efc341960f50ac8bde043bf8403b178a7a6ae
Jesus Morales <jesusalbertomoralesrico@gmail.com>
2022-08-05 20:03:44 -0500

    Update: Displayed more anime info

--- src/pages/AnimePage.vue
+++ src/pages/AnimePage.vue
@@ -2,9 +2,11 @@
   <div class="anime-page-container">
     <h1>Hello world anime page!</h1>
     <h3 v-if="isLoading">Loading...</h3>
-    <h3 v-else>
-      {{ anime?.title }}
-    </h3>
+    <div v-else>
+      <h1>{{ anime?.title }}</h1>
+      <p>{{ anime?.images }}</p>
+      <p>{{ anime?.synopsis }}</p>
+    </div>
   </div>
 </template>
 
