698a3bc6d65340569e0b2028669313fffe29451a
Jesus Morales <jesusalbertomoralesrico@gmail.com>
2022-08-07 16:48:37 -0500

    Feature: Added event to toggle sidebar

--- src/components/layout/AppHeader.vue
+++ src/components/layout/AppHeader.vue
@@ -1,7 +1,7 @@
 <template>
   <header class="header">
     <div class="logo-container">
-      <button class="menu-button">
+      <button @click="$emit('expandSidebar')" class="menu-button">
         <svg
           xmlns="http://www.w3.org/2000/svg"
           width="16"
