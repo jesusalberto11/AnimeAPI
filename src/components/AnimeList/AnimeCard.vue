81e31fd316de0ed9b4f9e59e8377dbc98523d7e3
Jesus Morales <jesusalbertomoralesrico@gmail.com>
2022-08-07 19:33:04 -0500

    Feature: Created anime card component

---
+++ src/components/AnimeList/AnimeCard.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="anime-card">
+    <img
+      v-bind:src="props?.imageSource"
+      v-bind:alt="props?.title"
+      v-bind:height="340"
+      v-bind:width="220"
+    />
+    <p class="anime-title">{{ props?.title }}</p>
+  </div>
+</template>
+
+<script setup>
+import { defineProps } from "vue";
+
+const props = defineProps({
+  imageSource: {
+    type: String,
+    required: true,
+  },
+  title: {
+    type: String,
+    required: true,
+  },
+});
+</script>
+
+<style scoped>
+.anime-card:hover {
+  box-shadow: 0 0 5px yellow, 0 0 5px yellowgreen, 0 0 10px yellow;
+}
+
+.anime-title {
+  background-color: brown;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  font-size: 18px;
+  text-align: center;
+  height: 50px;
+  width: 220px;
+}
+</style>
