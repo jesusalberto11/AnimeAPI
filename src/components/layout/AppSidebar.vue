e265694bb75c1de775205b0b40599cdbfca31524
Jesus Morales <jesusalbertomoralesrico@gmail.com>
2022-08-08 16:15:16 -0500

    Refractor: Added about link functionality

--- src/components/layout/AppSidebar.vue
+++ src/components/layout/AppSidebar.vue
@@ -94,26 +94,28 @@
       <a>Reviews</a>
     </div>
     <hr />
-    <div class="sidebar-item">
-      <div class="image">
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          width="16"
-          height="16"
-          fill="currentColor"
-          class="bi bi-info-circle"
-          viewBox="0 0 16 16"
-        >
-          <path
-            d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z"
-          />
-          <path
-            d="m8.93 6.588-2.29.287-.082.38.45.083c.294.07.352.176.288.469l-.738 3.468c-.194.897.105 1.319.808 1.319.545 0 1.178-.252 1.465-.598l.088-.416c-.2.176-.492.246-.686.246-.275 0-.375-.193-.304-.533L8.93 6.588zM9 4.5a1 1 0 1 1-2 0 1 1 0 0 1 2 0z"
-          />
-        </svg>
+    <router-link to="/about">
+      <div class="sidebar-item">
+        <div class="image">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="16"
+            height="16"
+            fill="currentColor"
+            class="bi bi-info-circle"
+            viewBox="0 0 16 16"
+          >
+            <path
+              d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z"
+            />
+            <path
+              d="m8.93 6.588-2.29.287-.082.38.45.083c.294.07.352.176.288.469l-.738 3.468c-.194.897.105 1.319.808 1.319.545 0 1.178-.252 1.465-.598l.088-.416c-.2.176-.492.246-.686.246-.275 0-.375-.193-.304-.533L8.93 6.588zM9 4.5a1 1 0 1 1-2 0 1 1 0 0 1 2 0z"
+            />
+          </svg>
+        </div>
+        <a>About</a>
       </div>
-      <a>Acerca de</a>
-    </div>
+    </router-link>
     <div class="sidebar-item">
       <div class="image">
         <svg
@@ -155,12 +157,17 @@ watch(route, (currentRoute) => {
   flex-direction: column;
 }
 
+.sidebar a {
+  text-decoration: none;
+}
+
 .sidebar-item {
   padding: 15px 35px;
   min-width: 800px;
   display: flex;
   flex-direction: row;
   gap: 40px;
+  color: #dfdfdf;
 }
 
 .image {
