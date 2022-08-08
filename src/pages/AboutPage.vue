7db73264b752323d347f3fba7a42ae983cd01dbb
Jesus Morales <jesusalbertomoralesrico@gmail.com>
2022-08-08 16:14:14 -0500

    Feature: Created about page

---
+++ src/pages/AboutPage.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="about-container">
+    <h1>Hello world aboutpage</h1>
+  </div>
+</template>
+
+<script setup></script>
+
+<style scoped>
+.about-container {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+</style>
