4c16d278a587917865db66ef3b23d58a3f9f2c29
Jesus Morales <jesusalbertomoralesrico@gmail.com>
2022-08-12 18:51:40 -0500

    Refractor: Splitted anime page Title into component

---
+++ src/components/AnimePage/AnimeDetailsTitle.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="anime-title-container">
+    <div class="anime-title">
+      <p v-if="props?.titleEnglish">
+        {{ props?.titleEnglish }}
+      </p>
+      <p v-else>{{ props?.title }}</p>
+    </div>
+  </div>
+</template>
+
+<script setup>
+import { defineProps } from "vue";
+
+const props = defineProps({
+  title: {
+    type: String,
+    required: true,
+  },
+  titleEnglish: {
+    type: String,
+  },
+});
+</script>
+
+<style scoped>
+.anime-title-container {
+  grid-area: title;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  padding: 15px 25px;
+}
+
+.anime-title {
+  text-align: center;
+  font-size: 24px;
+}
+</style>
