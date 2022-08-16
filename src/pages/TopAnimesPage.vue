a3e41982d633719b65bcd2450940f78c6307ae2c
Jesus Morales <jesusalbertomoralesrico@gmail.com>
2022-08-16 18:12:26 -0500

    Feat: Created top anime page

---
+++ src/pages/TopAnimesPage.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="top-animes-container">
+    <h1>Hello world top-animes</h1>
+  </div>
+</template>
+
+<script setup></script>
+
+<style scoped>
+.top-animes-container {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+</style>
