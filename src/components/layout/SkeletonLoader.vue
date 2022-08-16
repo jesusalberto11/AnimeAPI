a53a8a0d665029231651e2f7e9aa7e9da6e81d7d
Jesus Morales <jesusalbertomoralesrico@gmail.com>
2022-08-16 18:57:07 -0500

    Feat: Created skeleton loader component

---
+++ src/components/layout/SkeletonLoader.vue
@@ -0,0 +1,56 @@
+<template>
+  <div
+    class="skeleton"
+    :class="props?.type"
+    :style="{ height: props?.height, width: props?.width }"
+  ></div>
+</template>
+
+<script setup>
+import { defineProps } from "vue";
+
+const props = defineProps({
+  type: String,
+  height: String,
+  width: String,
+});
+</script>
+
+<style scoped>
+.box {
+  border-width: 2px;
+}
+
+.img {
+  border-radius: 50%;
+  height: 180px;
+  width: 180px;
+}
+
+.text {
+  width: 80px;
+  height: 0.5rem;
+  margin-bottom: 0.25rem;
+  border-radius: 0.125rem;
+}
+
+.skeleton-text:last-child {
+  margin-bottom: 0;
+  width: 80%;
+}
+
+.skeleton {
+  opacity: 0.7;
+  animation: skeleton-loading 1s linear infinite alternate;
+}
+
+@keyframes skeleton-loading {
+  0% {
+    background-color: hsl(200, 20%, 70%);
+  }
+
+  100% {
+    background-color: hsl(200, 20%, 95%);
+  }
+}
+</style>
