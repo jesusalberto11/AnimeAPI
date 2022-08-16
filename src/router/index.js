f71ac4415214ee18897fefe63972ac60cf0dee1a
Jesus Morales <jesusalbertomoralesrico@gmail.com>
2022-08-16 18:13:40 -0500

    Feat: Created top animes route

--- src/router/index.js
+++ src/router/index.js
@@ -3,6 +3,7 @@ import * as VueRouter from "vue-router";
 import HomePage from "../pages/HomePage.vue";
 import AnimeListPage from "../pages/AnimeListPage.vue";
 import AnimePage from "../pages/AnimePage";
+import TopAnimesPage from "../pages/TopAnimesPage.vue";
 import SearchResultsPage from "../pages/SearchResultsPage.vue";
 import AboutPage from "../pages/AboutPage.vue";
 import PathNotFound from "../pages/PathNotFound.vue";
@@ -11,6 +12,7 @@ const routes = [
   { path: "/", redirect: "/animes" },
   { path: "/home", name: "home", component: HomePage },
   { path: "/animes", name: "animes", component: AnimeListPage },
+  { path: "/top", name: "top", component: TopAnimesPage },
   { path: "/animes/anime/:id", name: "anime", component: AnimePage },
   { path: "/results", name: "results", component: SearchResultsPage },
   { path: "/about", name: "about", component: AboutPage },
