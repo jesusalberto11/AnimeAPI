242b4aa384ef952f3afb6a39636900327819cde4
Jesus Morales <jesusalbertomoralesrico@gmail.com>
2022-08-16 20:32:09 -0500

    Feat: Created manga list page

---
+++ src/pages/Manga/MangaPage.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="manga-page-container">
+    <h1>Hello world manga-page</h1>
+  </div>
+</template>
+
+<script setup></script>
+
+<style scoped>
+.manga-page-container {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+</style>
