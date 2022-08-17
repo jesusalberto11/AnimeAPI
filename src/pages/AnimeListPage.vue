4f78c42eaff47820d2455325da9e3cab83cb0f17
Jesus Morales <jesusalbertomoralesrico@gmail.com>
2022-08-16 19:20:23 -0500

    Feat: Added skeleton list while loading

--- src/pages/AnimeListPage.vue
+++ src/pages/AnimeListPage.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="page-container">
     <div v-if="isLoading">
-      <h1>Loading...</h1>
+      <SkeletonList />
     </div>
     <div v-else>
       <AnimeList :animes="animes" />
@@ -13,6 +13,7 @@
 import { onMounted } from "vue";
 import { useAnime } from "@/composable/useAnime.js";
 import AnimeList from "../components/AnimeList/AnimeList.vue";
+import SkeletonList from "@/components/Skeletons/SkeletonList.vue";
 
 const { animes, isLoading, fetchAnimeList } = useAnime();
 
