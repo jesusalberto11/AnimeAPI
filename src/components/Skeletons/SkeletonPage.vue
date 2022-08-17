f9f31e02ad976b51b5175971583a5b76d599f281
Jesus Morales <jesusalbertomoralesrico@gmail.com>
2022-08-16 19:59:13 -0500

    Feature: Created skeleton loader component for anime page

--- src/components/Skeletons/SkeletonPage.vue
+++ src/components/Skeletons/SkeletonPage.vue
@@ -1,15 +1,87 @@
 <template>
   <div class="skeleton-page-container">
-    <h1>Hello world skeleton-page</h1>
+    <div class="skeleton-title">
+      <SkeletonLoader :type="'text'" height="30px" width="95%" />
+    </div>
+    <div class="skeleton-logo">
+      <div class="logo-container">
+        <SkeletonLoader :type="'box'" height="100%" width="100%" />
+      </div>
+    </div>
+    <div class="skeleton-main-info">
+      <h1>LEL</h1>
+    </div>
+    <div class="skeleton-trailer">
+      <h1>LEL</h1>
+    </div>
+    <div class="skeleton-second-info">
+      <h1>LEL</h1>
+    </div>
   </div>
 </template>
 
-<script setup></script>
+<script setup>
+import SkeletonLoader from "../layout/SkeletonLoader.vue";
+</script>
 
 <style scoped>
 .skeleton-page-container {
+  height: 100%;
+  width: 100%;
+
+  display: grid;
+  grid-template-columns: 40% 60%;
+  grid-template-rows: auto 340px;
+  grid-template-areas:
+    "title title"
+    "logo maininfo"
+    "trailer secondinfo";
+}
+
+.skeleton-title {
+  grid-area: title;
   display: flex;
   align-items: center;
   justify-content: center;
+  padding: 15px 25px;
+}
+
+.skeleton-logo {
+  grid-area: logo;
+
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+
+.logo-container {
+  height: 340px;
+  width: 220px;
+}
+
+.skeleton-main-info {
+  grid-area: maininfo;
+  background-color: cadetblue;
+}
+
+.skeleton-trailer {
+  grid-area: trailer;
+  background-color: blue;
+}
+
+.skeleton-second-info {
+  grid-area: secondinfo;
+  background-color: brown;
+}
+
+@media screen and (max-width: 1348px) {
+  .skeleton-page-container {
+    grid-template-areas:
+      "title title"
+      "logo logo"
+      "maininfo maininfo"
+      "trailer trailer"
+      "secondinfo secondinfo";
+  }
 }
 </style>
