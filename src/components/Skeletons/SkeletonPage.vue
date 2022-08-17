2fe538d80b5d09aaa7d8ec7e4fa82f1744d3b28a
Jesus Morales <jesusalbertomoralesrico@gmail.com>
2022-08-16 19:34:41 -0500

    Feat: Created skeleton page component for anime page

---
+++ src/components/Skeletons/SkeletonPage.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="skeleton-page-container">
+    <h1>Hello world skeleton-page</h1>
+  </div>
+</template>
+
+<script setup></script>
+
+<style scoped>
+.skeleton-page-container {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+</style>
