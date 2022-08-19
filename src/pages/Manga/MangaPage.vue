c819763426bece83af6fc30cb3d39a777877aa69
Jesus Morales <jesusalbertomoralesrico@gmail.com>
2022-08-19 18:54:18 -0500

    Refractor: Changed top mangas and manga page layout

--- src/pages/Manga/MangaPage.vue
+++ src/pages/Manga/MangaPage.vue
@@ -1,34 +1,10 @@
 <template>
   <div class="manga-page-container">
-    <div class="manga-list">
-      <div v-for="manga in data?.data.data" :key="manga.mal_id">
-        <div class="manga-card">
-          <img
-            v-bind:src="manga?.images.jpg.image_url"
-            v-bind:alt="manga?.title"
-            v-bind:height="340"
-            v-bind:width="220"
-          />
-          <p class="manga-title">{{ manga?.title }}</p>
-        </div>
-      </div>
-    </div>
+    <h1>Hello from mang page!</h1>
   </div>
 </template>
 
-<script setup>
-import axios from "axios";
-import { onMounted, ref } from "vue";
-
-const data = ref(null);
-
-onMounted(() => {
-  axios
-    .get("https://api.jikan.moe/v4/manga")
-    .then((response) => (data.value = response))
-    .catch((error) => console.log("Error in MangaFetch: ", error));
-});
-</script>
+<script setup></script>
 
 <style scoped>
 .manga-page-container {
@@ -38,36 +14,5 @@ onMounted(() => {
   display: flex;
   justify-content: center;
   align-items: center;
-}
-
-.manga-list {
-  height: 100vh;
-  width: 100%;
-
-  display: flex;
-  flex-direction: row;
-  flex-wrap: wrap;
-  justify-content: center;
-  align-items: center;
-  gap: 25px;
-
-  padding-top: 25px;
-  padding-bottom: 100px;
-
-  overflow-y: scroll;
-}
-
-.manga-card {
-  height: 400px;
-  width: 220px;
-}
-
-.manga-title {
-  height: 60px;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
-  background-color: brown;
 }
 </style>
--- src/pages/Manga/TopMangasPage.vue
+++ src/pages/Manga/TopMangasPage.vue
@@ -2,15 +2,11 @@
   <div class="manga-page-container">
     <div class="manga-list">
       <div v-for="manga in data?.data.data" :key="manga.mal_id">
-        <div class="manga-card">
-          <img
-            v-bind:src="manga?.images.jpg.image_url"
-            v-bind:alt="manga?.title"
-            v-bind:height="340"
-            v-bind:width="220"
-          />
-          <p class="manga-title">{{ manga?.title }}</p>
-        </div>
+        <AnimeCard
+          :title="manga?.title"
+          :imageSource="manga?.images.jpg.image_url"
+          @click="onClickCard(manga?.mal_id)"
+        />
       </div>
     </div>
   </div>
@@ -19,8 +15,12 @@
 <script setup>
 import axios from "axios";
 import { onMounted, ref } from "vue";
+import { useRouter } from "vue-router";
+
+import AnimeCard from "@/components/AnimeList/AnimeCard.vue";
 
 const data = ref(null);
+const router = useRouter();
 
 onMounted(() => {
   axios
@@ -28,6 +28,13 @@ onMounted(() => {
     .then((response) => (data.value = response))
     .catch((error) => console.log("Error in MangaFetch: ", error));
 });
+
+const onClickCard = (mangaID) => {
+  router.push({
+    name: "manga",
+    params: { id: mangaID },
+  });
+};
 </script>
 
 <style scoped>
