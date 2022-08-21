19d5c23364b1bff1886e9c564b50a12f0c0f7142
Jesus Morales <jesusalbertomoralesrico@gmail.com>
2022-08-20 20:32:14 -0500

    Update: Changed page layout

--- src/pages/Manga/MangaPage.vue
+++ src/pages/Manga/MangaPage.vue
@@ -1,10 +1,28 @@
 <template>
   <div class="manga-page-container">
-    <h1>Ups! This isn't done yet 🥴🥴🥴🥴🥴🥴</h1>
+    <p>{{ manga?.title }}</p>
+    <p>Score: {{ manga?.score }}</p>
+    <p>Rank: {{ manga?.rank }}</p>
+    <p>{{ manga?.synopsis }}</p>
+    <p>{{ manga?.background }}</p>
   </div>
 </template>
 
-<script setup></script>
+<script setup>
+import axios from "axios";
+import { onMounted, ref } from "vue";
+import { useRoute } from "vue-router";
+
+const route = useRoute();
+const manga = ref(null);
+
+onMounted(() => {
+  axios
+    .get(`https://api.jikan.moe/v4/manga/${route.params?.id}/full`)
+    .then((response) => (manga.value = response.data.data))
+    .catch((error) => console.log("Error in fetchManga: ", error));
+});
+</script>
 
 <style scoped>
 .manga-page-container {
@@ -12,6 +30,7 @@
   width: 100%;
 
   display: flex;
+  flex-direction: column;
   justify-content: center;
   align-items: center;
 }
