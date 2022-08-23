47363bf032f1267b16f027fc26de2e32b8736e32
Jesus Morales <jesusalbertomoralesrico@gmail.com>
2022-08-23 15:04:47 -0500

    Update: Updated path not found styles

--- src/pages/PathNotFound.vue
+++ src/pages/PathNotFound.vue
@@ -1,7 +1,24 @@
 <template>
-  <h1>Hello world from path not found</h1>
+  <div class="path-not-found-container">
+    <h1>Error 404: Path not found</h1>
+    <h5>Why u're here?</h5>
+  </div>
 </template>
 
 <script setup></script>
 
-<style scoped></style>
+<style scoped>
+.path-not-found-container {
+  height: 100%;
+  width: 100%;
+
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  gap: 15px;
+
+  text-align: center;
+  padding: 10px 10px;
+}
+</style>
