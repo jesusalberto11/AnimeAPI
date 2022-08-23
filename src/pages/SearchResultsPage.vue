81f77e018668cb50dc8cd95d3a0c1e2134dd0d8d
Jesus Morales <jesusalbertomoralesrico@gmail.com>
2022-08-23 14:53:11 -0500

    Fix: Scroll behiavior now works

--- src/pages/SearchResultsPage.vue
+++ src/pages/SearchResultsPage.vue
@@ -1,12 +1,12 @@
 <template>
   <div class="search-results-container">
-    <div v-if="isLoading">
-      <h1>Loading...</h1>
+    <div v-if="isLoading" class="centered-text">
+      <h1>Searching...</h1>
     </div>
-    <div v-else-if="!isLoading && animes.length === 0">
-      <h1>Ups! No se ha encontrado nada con ese nombre</h1>
+    <div v-else-if="!isLoading && animes.length === 0" class="centered-text">
+      <h1>Ups! No results!</h1>
     </div>
-    <div v-else>
+    <div v-else class="list">
       <AnimeList :animes="animes" />
     </div>
   </div>
@@ -40,12 +40,24 @@ watch(route, (currentValue) => {
 
 <style scoped>
 .search-results-container {
-  height: 100%;
+  height: 100vh;
   width: 100%;
 
+  display: grid;
+  grid-template-rows: auto 100px;
+  grid-template-areas: "list";
+  overflow-y: scroll;
+}
+
+.centered-text {
+  height: 100%;
+  width: 100%;
   display: flex;
-  flex-direction: column;
-  justify-content: center;
   align-items: center;
+  justify-content: center;
+}
+
+.list {
+  grid-area: list;
 }
 </style>
