dc54bb8e4c699bdef2982e0d7b8385efbd249d5b
Jesus Alberto <jesusalbertomoralesrico@gmail.com>
2023-06-19 17:49:24 -0500

    src/Feat: App now uses global styles

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import { createApp } from "vue";
 import App from "./App.vue";
 import { createPinia } from "pinia";
 import { router } from "./router";
+import "./styles.css";
 
 const pinia = createPinia();
 const app = createApp(App);
